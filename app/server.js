4da165066cc686da19d461463c0c98799ccf602e
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-12 07:53:17 -0700

    deploy debug

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("../routes");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
@@ -8,14 +8,13 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.use(require("./routes/index.js"));
+app.use(require("../routes/index.js"));
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
