9118328a6144e0cc6de9de93537f1dda0cbca22e
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-12 07:30:24 -0700

    deploy debug att 2

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const db = require("./config/connection");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
 });
